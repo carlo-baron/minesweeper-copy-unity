f9e979d5687c0b53dcd07328b209344608595d2c
carlo-baron <carlobaron430@gmail.com>
2024-08-25 15:16:11 +0800

    Intital Committ
    contains basic tile maker and cell properties(reveal status, and bomb status, and neighbors)

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,59 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public bool isBomb = false;
+    public bool isRevealed = false;
+
+    private BoxCollider2D boxCollider2D;
+
+    public List<GameObject> neighbors { get; private set; } = new List<GameObject>();
+
+    public int surroundingBombs
+    {
+        get
+        {
+            int bombCount = 0;
+            foreach (GameObject cell in neighbors){
+                Cell cellScript = cell.GetComponent<Cell>();
+                if(cellScript.isBomb){
+                    bombCount++;
+                }
+            }
+
+            return bombCount;
+        }
+    }
+    void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Reveal();
+        }
+    }
+
+    public void Reveal()
+    {
+        isRevealed = true;
+        spriteRenderer.color = Color.blue;
+    }
+
+    List<GameObject> Neighbors()
+    {
+        LayerMask layer = gameObject.layer;
+
+        RaycastHit2D[] rays = Physics2D.BoxCastAll(boxCollider2D.bounds.center, boxCollider2D.size, 0f, Vector2.zero, Mathf.Infinity, layer);
+
+        return rays.Where(hit => hit.collider.CompareTag("Tile")).Select(hit => hit.collider.gameObject).ToList();
+    }
+}
---
+++ Assets/Scripts/Tilemaker.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+
+public class Tilemaker : MonoBehaviour{
+    public GameObject cellPrefab;
+    private float cellSize = -1f;
+
+    public uint rows;
+    public uint columns;
+    public uint maxBombs;
+    private uint currentBombs = 0;
+
+    private GameObject[,] cellGrid;
+
+    void Awake(){
+        cellGrid = new GameObject[rows, columns];
+
+        for(int i = 0; i < rows; i++){
+            for(int j = 0; j < columns; j++){
+                Vector2 position = new Vector2(i * cellSize, j * cellSize);
+                cellGrid[i,j] = Instantiate(cellPrefab, position, Quaternion.identity, transform);
+            }
+        }
+
+        //position the object to the upper right of the screen
+        transform.position = new Vector3(4.5f, 4.5f, transform.position.z);
+        RandomizedBomb();
+    }
+
+    void RandomizedBomb(){
+        while (currentBombs != maxBombs){
+            int randomRow = Mathf.FloorToInt(Random.Range(0, rows));
+            int randomColumn = Mathf.FloorToInt(Random.Range(0, columns));
+
+            Cell randomCell = cellGrid[randomRow, randomColumn].GetComponent<Cell>();
+            if(BombRulesCheck(randomCell)){
+                MakeBomb(randomCell);
+                currentBombs++;
+            } 
+        }
+    }
+
+    void MakeBomb(Cell cellScript){
+        cellScript.isBomb = true;
+        cellScript.spriteRenderer.color = Color.red;
+    }
+
+    bool BombRulesCheck(Cell cellScript){
+        return cellScript.surroundingBombs <= 2;
+    }
+}
